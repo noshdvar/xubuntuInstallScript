0a6629f7c3505fd6cc357bc6e4e7db46033b6977
Sönke <5450979+noshdvar@users.noreply.github.com>
2026-08-10 09:48:05 +0000

    Update install_deb_kdeapps.sh

--- install_deb_kdeapps.sh
+++ install_deb_kdeapps.sh
@@ -28,17 +28,13 @@ sudo apt install -y \
     zip \
     fonts-jetbrains-mono
 
-
 echo "=== Flathub aktivieren ==="
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 
 echo "=== KDE / Debian Repo Anwendungen installieren ==="
 sudo apt install -y \
     kde-cli-tools \
-    krename \
     kio-extras \
-    kdiff3 \
     kate \
     partitionmanager \
     haruna \
@@ -46,12 +42,9 @@ sudo apt install -y \
     audacity \
     rustdesk \
     ktorrent \
-    btop \
-    kdeconnect \
+    fastfetch \
     yakuake \
     k3b \
-    umbrello \
-    kdenlive \
     krita \
     tellico \
     kget \
@@ -59,10 +52,8 @@ sudo apt install -y \
     kasts \
     skrooge \
     akregator \
-    ksnip \
     minder \
     krusader \
-    thunderbird \
     teamviewer \
     audex
 
@@ -76,22 +67,17 @@ sudo apt update
 # Install Microsoft Edge
 sudo apt install microsoft-edge-stable
 
-
 echo "=== LibreWolf Repository hinzufügen ==="
-wget -O- https://deb.librewolf.net/keyring.gpg | \
-    sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/librewolf.gpg] https://deb.librewolf.net bookworm main" | \
-    sudo tee /etc/apt/sources.list.d/librewolf.list
-
+sudo apt update
+sudo apt install extrepo -y
+sudo extrepo enable librewolf
+sudo extrepo update librewolf
 sudo apt update
 sudo apt install -y librewolf
 
-
 echo "=== Flatpak Anwendungen installieren ==="
 flatpak install -y flathub \
-    app.drey.EarTag \
+    io.github.mrvladus.List
     com.spotify.Client \
     io.anytype.anytype \
     org.ksnip.ksnip \
@@ -100,15 +86,12 @@ flatpak install -y flathub \
     au.com.shiftyjelly.pocketcasts \
     com.github.jeromerobert.pdfarranger \
     im.riot.Riot \
-    com.bitwarden.desktop \
     io.github.kdiskmark.KDiskMark \
     org.telegram.desktop
-
 
 echo "=== Helium ==="
 echo "Bitte manuell installieren (nicht sauber paketiert für Debian/MX):"
 echo "https://helium.computer"
-
 
 echo "=== Installation abgeschlossen ==="
 echo "Bitte einmal neu anmelden oder rebooten."
